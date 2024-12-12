1bf6e6e27af01af6b478d195c57d8a45633a6bce
kamaubryan <briankamau344@gmail.com>
2024-12-12 18:00:17 +0300

    added a getallCars method

--- src/main/java/com/example/carCrud/Controller/CarController.java
+++ src/main/java/com/example/carCrud/Controller/CarController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/")// here we are specifying where the api will start
 public class CarController {
@@ -25,6 +27,17 @@ public class CarController {
             throw new RuntimeException(e);
         }
     }
+    // getting all the items
+    @GetMapping("/")
+    public ResponseEntity<List<Car>> getAllProducts() {
+    try {
+        List<Car>  Cars = carService.getAllCars();
+        return new ResponseEntity<>(Cars, HttpStatus.FOUND);
+    } catch (RuntimeException e) {
+        throw new RuntimeException( "your cars are not available", e);
+    }
+
+    }
 
     @PutMapping("update")
     public ResponseEntity<?> updateCar(@RequestParam Long id, @RequestBody CarDto car) {
@@ -32,8 +45,5 @@ public class CarController {
         return new ResponseEntity<>(carService.updateCar(id, car), HttpStatus.OK);
 
     }
-    @GetMapping("cars")
-    public ResponseEntity<?> getAllCars() {
-        return new ResponseEntity<>(carService.getAllCars(), HttpStatus.OK);
-    }
+
 }
