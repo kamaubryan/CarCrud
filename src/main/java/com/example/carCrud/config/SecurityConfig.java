18b06ee63a17307003e15ebe4ab9797e01853c5d
kamaubryan <briankamau344@gmail.com>
2024-12-14 22:47:01 +0300

    modified the maven file and also the controller file but the security is bring issues

--- src/main/java/com/example/carCrud/config/SecurityConfig.java
+++ src/main/java/com/example/carCrud/config/SecurityConfig.java
@@ -1,46 +1,46 @@
-package com.example.carCrud.config;
-
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig{
-  public   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-      http.cors(Customizer.withDefaults());
-      http.csrf(Customizer.withDefaults());
-      http.authorizeHttpRequests(
-              auth->auth
-                      .requestMatchers("/cars").hasRole("ADMIN")
-                      .requestMatchers("/login").permitAll()
-
-
-
-      );
-      return http.build();
-
-  }
-
-  @Bean
-    InMemoryUserDetailsManager userDetailsManager() {
-
-
-
-     UserDetails admin = User.withDefaultPasswordEncoder().password("one").username("kamau").roles("ADMIN").build();
-     UserDetails user= User.withDefaultPasswordEncoder().password("one").username("user").roles("USER").build();
-
-     return new InMemoryUserDetailsManager(user, admin);
-
-  }
-
-}
-
+//package com.example.carCrud.config;
+//
+//import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig{
+//  public   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//      http.cors(Customizer.withDefaults());
+//      http.csrf(Customizer.withDefaults());
+//      http.authorizeHttpRequests(
+//              auth->auth
+//                      .requestMatchers("/cars").hasRole("ADMIN")
+//                      .requestMatchers("/login").permitAll()
+//
+//
+//
+//      );
+//      return http.build();
+//
+//  }
+//
+//  @Bean
+//    InMemoryUserDetailsManager userDetailsManager() {
+//
+//
+//
+//     UserDetails admin = User.withDefaultPasswordEncoder().password("one").username("kamau").roles("ADMIN").build();
+//     UserDetails user= User.withDefaultPasswordEncoder().password("one").username("user").roles("USER").build();
+//
+//     return new InMemoryUserDetailsManager(user, admin);
+//
+//  }
+//
+//}
+//
